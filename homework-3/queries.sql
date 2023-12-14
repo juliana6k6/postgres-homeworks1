d5ceb79d195a92a867d6a18a7889ec12a0936a52
Juliana <juliana6k@mail.ru>
2023-12-14 11:57:49 +0300

    HW3

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -1,13 +1,26 @@
 -- Напишите запросы, которые выводят следующую информацию:
 -- 1. Название компании заказчика (company_name из табл. customers) и ФИО сотрудника, работающего над заказом этой компании (см таблицу employees),
 -- когда и заказчик и сотрудник зарегистрированы в городе London, а доставку заказа ведет компания United Package (company_name в табл shippers)
-
+select customers.company_name as customer,
+CONCAT(employees.first_name, ' ', employees.last_name) as employee
+from orders
+INNER JOIN employees USING(employee_id)
+INNER JOIN customers USING(customer_id)
+INNER JOIN shippers on orders.ship_via=shippers.shipper_id
+where customers.city = 'London' and
+employees.city = 'London' and shippers.company_name = 'United Package'
 
 -- 2. Наименование продукта, количество товара (product_name и units_in_stock в табл products),
 -- имя поставщика и его телефон (contact_name и phone в табл suppliers) для таких продуктов,
 -- которые не сняты с продажи (поле discontinued) и которых меньше 25 и которые в категориях Dairy Products и Condiments.
 -- Отсортировать результат по возрастанию количества оставшегося товара.
-
+select product_name, units_in_stock, contact_name, phone
+from products
+join suppliers using(supplier_id)
+join categories using(category_id)
+where discontinued <> 1 and units_in_stock < 25
+and category_name in ('Dairy Products', 'Condiments')
+order by units_in_stock
 
 -- 3. Список компаний заказчиков (company_name из табл customers), не сделавших ни одного заказа
 
